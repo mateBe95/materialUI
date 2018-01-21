f82871d27b40b8daea8b4e7dc836ad671105c485
insomnia1337 <mateusz.bieda95@gmail.com>
2018-01-21 20:04:53 +0100

    add menu drawe

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  handleClick(){
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+   handleClose() {
+  this.setState({
+    open: false
+  })
+   } 
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonClick	= {() => this.handleClick()}
+            
+          />
+          <Drawer 
+          open={this.state.open}
+          docked={false}
+          onRequestChange={(open)=>this.setState({open})}
+          >
+          <MenuItem onClick={()=> this.handleClose()}>Menu Item</MenuItem>
+          <MenuItem onClick={()=> this.handleClose()}>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
